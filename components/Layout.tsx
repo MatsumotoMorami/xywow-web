9a4b55650995af45f1a9ac9e68ff8c1a5a4a0ddc
Kcalb_MengWang <kcalb_mengwang@kcalbmengwang.com>
2024-12-05 08:33:51 +0800

    Update Layout.tsx

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -5,12 +5,14 @@ import 'rsuite/dropdown/styles/index.css'
 import 'rsuite/Panel/styles/index.css'
 import DashboardIcon from '@rsuite/icons/legacy/Dashboard';
 import CalendarIcon from '@rsuite/icons/legacy/Calendar';
-import {useState} from "react";
+import { useEffect, useState} from "react";
 import {Paypal, Server} from "@rsuite/icons/cjs/react/legacy";
 import MenuIcon from '@rsuite/icons/Menu';
 import { Text } from 'rsuite';
 import { motion } from "framer-motion";
 export function DefaultLayout() {
+
+    const [nickname, setNickname] = useState("忆梦");
     const [expand, setExpand] = useState(false);
     const [page, setPage] = useState(1);
     const onclick1=()=>{setPage(1)}
@@ -18,6 +20,29 @@ export function DefaultLayout() {
     const onclick3=()=>{setPage(3)}
     const onclick4=()=>{setPage(4)}
     const onclick5=()=>{setPage(5)}
+
+
+    const SERVER_URL = "https://frp-fly.top:12060";
+
+    useEffect(() => {
+        // 获取用户信息
+        fetch(`${SERVER_URL}/auth/hello`, {
+            method: 'GET',
+            credentials: 'include' // 确保发送cookie
+        })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Failed to fetch user info');
+                }
+                return response.json();
+            })
+            .then(data => {
+                setNickname(data.userName);
+            })
+            .catch(error => {
+                console.error('Error fetching user info:', error);
+            });
+    }, []);
     // @ts-ignore
     return (
         <Container>
@@ -43,7 +68,7 @@ export function DefaultLayout() {
                             alt="头像"
                     ></img>
                     <Panel header={<Text weight={"extrabold"}>个人信息</Text>} shaded className={"bg-white w-[20vw] h-[20vh] ml-5"}>
-                        <Text>昵称：{"忆梦"}</Text>
+                        <Text>昵称：{nickname}</Text>
                         <Text>状态：{"在店10小时26分钟"}</Text>
                         <Text className={"whitespace-pre-wrap"}>预计消费：{"￥80"}    余额：{"￥100"}</Text>
                         <Text></Text>
